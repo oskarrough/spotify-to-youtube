9e55ba437c33f530089d08053f99dbae5a5b3209
oskarrough <oskar@rough.dk>
2024-06-27 19:28:14 +0200

    Add new <text-to-youtube> component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import 'https://cdn.jsdelivr.net/npm/@radio4000/components/dist/r4.js'
 import 'https://cdn.jsdelivr.net/gh/oskarrough/rough-spinner/rough-spinner.js'
 
 import SpotifyToYoutube from './spotify-to-youtube.js'
+import TextToYoutube from './text-to-youtube.js'
 import R4BatchImport from './r4-batch-import.js'
 
 customElements.define('spotify-to-youtube', SpotifyToYoutube)
+customElements.define('text-to-youtube', TextToYoutube)
 customElements.define('r4-batch-import', R4BatchImport)
